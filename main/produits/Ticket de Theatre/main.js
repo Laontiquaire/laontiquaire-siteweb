f7ea16edd149abffa5c8d93fc3f68cf84191bae4
Raphaël <126982829+raphrapide100@users.noreply.github.com>
2024-11-25 11:49:06 +0100

    Theatre ah cringe

---
+++ main/produits/Ticket de Theatre/main.js
@@ -0,0 +1,5 @@
+document.getElementById('buy-button').addEventListener('click', function () {
+    alert("Merci pour votre intérêt ! Vous serez redirigé vers la page de paiement.");
+    // Redirection possible ici
+    // window.location.href = "/paiement";
+});
