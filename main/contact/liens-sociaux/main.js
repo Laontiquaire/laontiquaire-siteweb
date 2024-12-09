d4f9dadc8b5b04d4a4590f0479a42fc850606ad2
Raphaël <126982829+raphrapide100@users.noreply.github.com>
2024-12-09 12:21:28 +0100

    Up

---
+++ main/contact/liens-sociaux/main.js
@@ -0,0 +1,4 @@
+// Fonction pour rediriger vers l'accueil
+function goToHome() {
+    window.location.href = '/'; // Remplacez par l'URL de votre page d'accueil si nécessaire
+}
--- main/produits/tdt/main.js
+++ main/produits/tdt/main.js
@@ -1,11 +1,12 @@
+// Gestion du bouton d'achat
 document.getElementById('buy-button').addEventListener('click', function () {
     alert("Merci pour votre intérêt ! Vous serez redirigé vers la page de paiement.");
     // Redirection possible ici
     // window.location.href = "/paiement";
 });
 
+// Gestion du bouton retour à l'accueil
 document.getElementById('back-home-button').addEventListener('click', function () {
-    alert("Retour à l'accueil.");
     // Redirection vers la page d'accueil
-    window.location.href = "/";
+    window.location.href = "/produits/";
 });
