2d7feed6bae16f2317f9ddb0ba83e38d7a2e26d7
Raphaël <126982829+raphrapide100@users.noreply.github.com>
2025-03-31 12:05:51 +0200

    Patch the button

--- main/produits/adm/main.js
+++ main/produits/adm/main.js
@@ -1,10 +1,3 @@
-// Gestion du bouton d'achat
-document.getElementById('buy-button').addEventListener('click', function () {
-    alert("Merci pour votre intérêt ! Vous serez redirigé vers la page de paiement.");
-    // Redirection possible ici
-    // window.location.href = "/paiement";
-});
-
 // Gestion du bouton retour à l'accueil
 document.getElementById('back-home-button').addEventListener('click', function () {
     // Redirection vers la page d'accueil
