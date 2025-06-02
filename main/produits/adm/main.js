c27e571bd47c71792f1fdc5fef5c1dc609918cea
Raphaël <126982829+raphrapide100@users.noreply.github.com>
2025-06-02 11:51:01 +0200

    UPDATE VISUEL BOY

--- main/produits/adm/main.js
+++
@@ -1,5 +0,0 @@
-// Gestion du bouton retour à l'accueil
-document.getElementById('back-home-button').addEventListener('click', function () {
-    // Redirection vers la page d'accueil
-    window.location.href = "/produits/";
-});
